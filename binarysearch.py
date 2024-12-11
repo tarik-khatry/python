1234c9cb80047a901b988336b8b087107486888a
tarik-khatry <mohammedtarik94@gmail.com>
2024-12-12 00:15:16 +0530

    binary search recursion

--- binarysearch.py
+++ binarysearch.py
@@ -1,4 +1,4 @@
-def binary_search(l,k):
+'''def binary_search(l,k):
     begin=0
     end=len(l)-1
     while(end-begin>1):
@@ -14,6 +14,31 @@ def binary_search(l,k):
     else:
         return 0
 
-l=[2,4,5,2,1,25,6,7,4,2,2,6,7,8]
+l=list(range(1000))
 l.sort()
-print(binary_search(l,25))
+print(binary_search(l,998))'''
+
+#Using Recursion
+def recursive_binary_search(l,k,begin,end):
+    if(begin==end):
+        if (l[begin]==k):
+            return 1
+        else:
+            return 0
+    if (end-begin==1):
+        if (l[begin]==k) or (l[end]==k):
+            return 1
+    if (end-begin<0):
+        return 0
+    if (end-begin>1):
+        mid=(begin+end)//2
+        if (l[mid]>k):
+            end=mid-1
+        if (l[mid]<k):
+            begin=mid+1
+        if (l[mid]==k):
+            return 1
+    return recursive_binary_search(l,k,begin,end)
+
+l=list(range(1000))
+print(recursive_binary_search(l,235,0,len(l)-1))
