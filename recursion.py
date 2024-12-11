05155305172db8dfac4437fc582cc3773a7ec6eb
tarik-khatry <mohammedtarik94@gmail.com>
2024-12-11 16:48:58 +0530

    message:    changes in lessons

--- recursion.py
+++ recursion.py
@@ -1,8 +1,17 @@
 #recursion lessons iitm
 
-def compound(p,y):
+'''def compound(p,y):
     if (y==1):
         return p*1.1
     else:
         return compound(p,y-1)*1.1 
-print(compound(1000,50))       
+print(compound(1000,50))       '''
+
+#check 0  in a list
+l=[3,2,5,6,2,3,5,6,0,5,53,6]
+def check0(n):
+    if (n[0]==0):
+        return 1    
+    else: 
+        return check0(n[1:])
+print(check0(l))
